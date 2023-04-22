66745a4402651524e0e9cd3b308a0d598a4bc3b7
Diego Coscolla <diegocoscolla@gmail.com>
2023-04-22 19:00:24 -0300

    Linter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import { type AppType } from "next/app";
+import {type AppType} from "next/app";
 
-import { api } from "~/utils/api";
+import {api} from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({Component, pageProps}) => {
   return <Component {...pageProps} />;
 };
 
