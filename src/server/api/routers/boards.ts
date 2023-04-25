cdef47f50eaff5cf3f1db530007fa2d4aebc0db7
Diego Coscolla <diegocoscolla@gmail.com>
2023-04-25 00:18:04 -0300

    Add tasks count per board to board getAll

--- src/server/api/routers/boards.ts
+++ src/server/api/routers/boards.ts
@@ -3,7 +3,28 @@ import {z} from "zod";
 import {createTRPCRouter, privateProcedure} from "~/server/api/trpc";
 
 export const boardsRouter = createTRPCRouter({
-  getAll: privateProcedure.query(({ctx}) => {
-    return ctx.prisma.board.findMany({where: {authorId: ctx.currentUser}});
+  getAll: privateProcedure.query(async ({ctx}) => {
+    const boards = await ctx.prisma.board.findMany({
+      where: {
+        authorId: ctx.currentUser,
+      },
+    });
+
+    const tasks = await ctx.prisma.task.findMany({
+      where: {
+        authorId: ctx.currentUser,
+      },
+    });
+
+    const boardsWithTaskCount = boards.map((board) => {
+      const taskCount = tasks.filter((task) => task.boardId === board.id).length;
+
+      return {
+        ...board,
+        taskCount,
+      };
+    });
+
+    return boardsWithTaskCount;
   }),
 });
