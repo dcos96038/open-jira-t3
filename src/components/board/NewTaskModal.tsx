c43eeaf0129d10485cd68d9ae1e301cb1e393288
Diego Coscolla <diegocoscolla@gmail.com>
2023-04-29 13:25:30 -0300

    Refactor with new modalWrapper component

--- src/components/board/NewTaskModal.tsx
+++ src/components/board/NewTaskModal.tsx
@@ -5,6 +5,7 @@ import React, {useState} from "react";
 import {api} from "~/utils/api";
 
 import {Button} from "../ui/Button";
+import {ModalWrapper} from "../ui/ModalWrapper";
 
 interface Props {
   setCreateTaskModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -49,66 +50,64 @@ export const NewTaskModal: React.FC<Props> = ({setCreateTaskModalIsOpen}) => {
   };
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black/50">
-      <div className="flex h-full w-full flex-col gap-4 rounded-md bg-slate-900 p-4 shadow-xl sm:h-min sm:w-min sm:min-w-[400px]">
-        <div className="flex items-center justify-between py-1">
-          <h2 className="text-lg font-bold text-orange-500">Create new task</h2>
-          <Button
-            className="flex h-5 w-5 items-center justify-center rounded-md bg-orange-500 text-white"
-            disabled={isLoading}
-            onClick={() => setCreateTaskModalIsOpen(false)}
-          >
-            X
+    <ModalWrapper>
+      <div className="flex items-center justify-between py-1">
+        <h2 className="text-lg font-bold text-orange-500">Create new task</h2>
+        <Button
+          className="flex h-5 w-5 items-center justify-center rounded-md bg-orange-500 text-white"
+          disabled={isLoading}
+          onClick={() => setCreateTaskModalIsOpen(false)}
+        >
+          X
+        </Button>
+      </div>
+      <div className="flex flex-col gap-3">
+        <label className="text-sm font-semibold text-white" htmlFor="title">
+          Title
+        </label>
+        <input
+          className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
+          id="title"
+          placeholder="Enter some title..."
+          type="text"
+          value={formValues.title}
+          onChange={(e) => setFormValues({...formValues, title: e.target.value})}
+        />
+        <label className="text-sm font-semibold text-white" htmlFor="state">
+          State
+        </label>
+        <select
+          className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
+          id="state"
+          value={formValues.state}
+          onChange={(e) => setFormValues({...formValues, state: e.target.value as Task["state"]})}
+        >
+          {OPTIONS.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <label className="text-sm font-semibold text-white" htmlFor="content">
+          Content
+        </label>
+        <textarea
+          className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
+          id="content"
+          placeholder="Enter some content..."
+          rows={3}
+          value={formValues.content}
+          onChange={(e) => setFormValues({...formValues, content: e.target.value})}
+        />
+        <div className="flex justify-end gap-4">
+          <Button disabled={isLoading} onClick={() => setCreateTaskModalIsOpen(false)}>
+            Cancel
+          </Button>
+          <Button isLoading={isLoading} onClick={() => onCreate()}>
+            Create
           </Button>
         </div>
-        <div className="flex flex-col gap-3">
-          <label className="text-sm font-semibold text-white" htmlFor="title">
-            Title
-          </label>
-          <input
-            className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
-            id="title"
-            placeholder="Enter some title..."
-            type="text"
-            value={formValues.title}
-            onChange={(e) => setFormValues({...formValues, title: e.target.value})}
-          />
-          <label className="text-sm font-semibold text-white" htmlFor="state">
-            State
-          </label>
-          <select
-            className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
-            id="state"
-            value={formValues.state}
-            onChange={(e) => setFormValues({...formValues, state: e.target.value as Task["state"]})}
-          >
-            {OPTIONS.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-          <label className="text-sm font-semibold text-white" htmlFor="content">
-            Content
-          </label>
-          <textarea
-            className="rounded-md bg-zinc-800 px-3 py-2 text-white outline-none"
-            id="content"
-            placeholder="Enter some content..."
-            rows={3}
-            value={formValues.content}
-            onChange={(e) => setFormValues({...formValues, content: e.target.value})}
-          />
-          <div className="flex justify-end gap-4">
-            <Button disabled={isLoading} onClick={() => setCreateTaskModalIsOpen(false)}>
-              Cancel
-            </Button>
-            <Button isLoading={isLoading} onClick={() => onCreate()}>
-              Create
-            </Button>
-          </div>
-        </div>
       </div>
-    </div>
+    </ModalWrapper>
   );
 };
